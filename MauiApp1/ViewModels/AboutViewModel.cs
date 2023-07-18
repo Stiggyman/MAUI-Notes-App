78f7f4ba61daa7ef42cdfe1b606b236419e95b41
Stig Veje Mogensen <svmo@ibc.dk>
2023-07-18 13:33:43 +0200

    Working MVVM pattern project using MAUI

--- MauiApp1/ViewModels/AboutViewModel.cs
+++ MauiApp1/ViewModels/AboutViewModel.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
 
-namespace MauiApp1.Models
+namespace MauiApp1.ViewModels
 {
-    internal class About
+    internal class AboutViewModel
     {
         public string Title => AppInfo.Name;
         public string Version => AppInfo.VersionString;
         public string MoreInfoUrl => "https://aka.ms/maui";
         public string Message => "This app is written in XAML and C# using .NET MAUI.";
+        public ICommand ShowMoreInfoCommand { get;}
 
+
+        public AboutViewModel()
+        {
+            ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+        }
+
+        async Task ShowMoreInfo() => await Launcher.OpenAsync(MoreInfoUrl);
     }
 }
--- MauiApp1/Views/NotePage.xaml.cs
+++ MauiApp1/Views/NotePage.xaml.cs
@@ -2,61 +2,12 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace MauiApp1.Views;
 
-[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage 
 {
-	
-	
+
+
 	public NotePage()
 	{
 		InitializeComponent();
-
-		string appDataPath = FileSystem.AppDataDirectory;
-		string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
-		
-		LoadNote(Path.Combine(appDataPath, randomFileName));
-	}
-	public string ItemId
-	{
-        set
-		{
-            LoadNote(value);
-        }
-    }
-	private async void SaveButton_Clicked(object sender, EventArgs e)
-	{
-		if (BindingContext is Models.Note note)
-		{
-			  File.WriteAllText(note.Filename, note.Text);
-		}
-        await Shell.Current.GoToAsync("..");
-    }
-
-	private async void DeleteButton_Clicked(object sender, EventArgs e)
-	{
-		if (BindingContext is Models.Note note)
-		{
-			if (File.Exists(note.Filename))
-			{
-				File.Delete(note.Filename);
-			}
-		
-		}
-
-		await Shell.Current.GoToAsync("..");
-    }
-
-	private void LoadNote(string fileName)
-	{
-		Models.Note noteModel = new Models.Note();
-		noteModel.Filename = fileName;
-
-		if (File.Exists(fileName))
-		{
-			noteModel.Date = File.GetCreationTime(fileName);
-			noteModel.Text = File.ReadAllText(fileName);
-		}
-
-		BindingContext = noteModel;
 	}
 }
